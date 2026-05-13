46888f34d3b52547a96a4432f67aef56efef032f
Henrique Blay Barboza <38546106+TheBlay@users.noreply.github.com>
2026-05-13 14:01:15 -0300

    Adicionada a base do projeto
    Adicionados ícone, folha de estilo, cabeçalho modularizado, index e páginas de navegação.
    Por enquanto como template e como exemplo.
    
    Estrutura inteira será alterada

---
+++ header.php
@@ -0,0 +1,139 @@
+<head>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<script>localStorage.clear();
+
+const topBtn = document.getElementById('topBtn');
+
+    window.addEventListener("scroll", () => {
+        if (document.documentElement.scrollTop > 100) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
+        });
+
+    topBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    });
+
+</script>
+<style>
+:root {
+    --cor-de-fundo-container-nav: #FCE7C870;
+    --cor-fundo-navbar: #C9B59C;
+    --cor-botoes-nav: #DCC5B2;
+    --fonte-botoes-nav: "Chango, sans-serif";
+    --cor-fonte-botoes-nav: black;
+}
+
+.navBarContainer {
+    display: grid;
+    grid-auto-flow: column;
+    background-color: var(--cor-de-fundo-container-nav);
+    gap:5em;
+    max-width: max-content;
+    min-width: 100%;
+    min-height: 60px;
+    justify-content: normal; /* space-around, strecth and normal*/
+    
+}
+
+.navBarSuperior, .configBarSuperior {
+    max-width: 100dvh;
+    min-width: 50pc;
+    flex-wrap: wrap;
+    height: 60px;
+    border-radius:10px;
+    font-family: var(--fonte-botoes-nav);
+    background-color: var(--cor-fundo-navbar, #D2C4B4);
+    justify-content: space-around;
+    align-content: space-around;
+    display: flex;
+    gap: 5.5em;
+    
+}
+
+.configBarSuperior {
+min-width: max-content;
+display: inline-block;
+max-width: 30%;
+}
+
+.contain, .configBtn {
+    border: 2px solid black;
+    flex-wrap: wrap;
+    text-decoration: none;
+    font-size: 1.7em;
+    height: 70%;
+    width: 14%;
+    color: var(--cor-fonte-botoes-nav, black);
+    background-color: var(--cor-botoes-nav);
+    border-radius: 15px;
+    justify-content: space-around;
+    align-content: space-evenly;
+    display: inline-flex;
+}
+
+.configBtn {
+    width: fit-content;
+    
+}
+
+img {
+  border-radius: 5px;
+  width: 60px;
+  height: 60px;
+}
+
+#topBtn {
+      position: fixed;
+      bottom: 15px;
+      right: 15px;
+      display: block;
+      padding: 10px 15px;
+      background: #dd7514;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      text-decoration: none;
+    }
+
+.contain:hover, #topBtn:hover{
+    background-color: #dea575;
+    text-shadow: #fcfbfa;
+}
+
+.navBarContainer{
+    position: fixed;
+    
+}
+
+a:not(.contain) {
+  
+  margin-block: 1px;
+  
+}
+
+
+ </style>
+
+<div class="navBarContainer"><img src="favicon.ico">
+    <div class="navBarSuperior">
+        <a class="contain" href="index.php" >Home</a>
+        <a class="contain" href="peixaria.php" >Peixaria</a>
+        <a class="contain" href="filmesFavoritos.php" >Filmes</a>
+    </div>
+    <div class="configBarSuperior">
+        <button class="configBtn" type="reset">Limpar campos</button>
+        <button class="configBtn"onClick="#">Config.</button>
+    </div>
+</div>
+<hr/>
+<a id="topBtn" href="#" >Topo da Página</a>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+
+?>
+<!--
+ Autoria de Henrique Blay Barboza  |  RA: 1290482612001
+ Confira no GitHub! -> https://github.com/TheBlay/HenriqueBlay-LP420261_P2
+
+b) O modelo propõe a “Divulgação” de diversas
+“Publicações”.
+c) Entenda uma publicação, por exemplo, como um
+livro que está sendo publicado e que há um evento de
+divulgação deste livro.
+d) Os “Autores” possuem tipos de classificação como
+Coautores, Revisores, etc.
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Publicações</title>
+</head>
+
+
+<body>
+<?php  include_once 'header.php' ?>
+<hr/><div class='title'>Produtos</div>
+	<div class="caixa">
+            <input type="text" id="nome" name="nome" placeholder="Nome do Produto">
+            <textarea rows="6" cols="50 id="descricao" name="descricao" placeholder="Descrição do Produto"></textarea>
+            <input type="number" id="preco" name="preco" type="number" step="0.01" placeholder="Preço do Produto">
+            <button onclick="cadastrarProduto()">Adicionar produto</button>
+            
+    </div>
+	
+        <div class="caixa lista" id="listaProdutos"></div>
+
+
+    <script>
+
+     const listaP = document.getElementById('listaProdutos');
+
+         function cadastrarProduto()
+        {
+             const nome = document.getElementById('nome').value;
+             const descricao = document.getElementById('descricao').value;
+             const preco = document.getElementById('preco').value;
+
+             const body = new URLSearchParams({
+                nome: nome,
+                descricao: descricao,
+                preco: preco
+            });
+            fetch('produtos.php', 
+            {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                    body: body.toString()
+                })
+                .then(response => response.json())
+                .then(produto => {
+                    // Atualiza o conteúdo da div com a lista atualizada; tb usado no carregarProduto
+                    listaProdutos.innerHTML =
+                    produto.map(p => `<div class="elemento">Produto: <br>
+                    Nome: ${p.nome} <br>
+                    Descrição: ${p.descricao} <br>
+                    Preço: R$ ${p.preco} </div><hr/>
+                `).join('');
+                })
+                .catch(error => console.error('Erro:', error));
+
+                
+        }
+
+        function carregarProdutos()
+         { //Exibir
+            fetch('produtos.php', { method: 'GET' })
+                .then(response => response.json())
+                .then(produto => {
+                    document.getElementById('listaProdutos').innerHTML =
+                    produto.map(p => `<div class="elemento">Produto: <br>
+                    Nome: ${p.nome} <br>
+                    Descrição: ${p.descricao} <br>
+                    Preço: R$ ${parseFloat(p.preco)} </div><hr/>
+                `).join('');
+                })
+                .catch(error => console.error('Erro ao carregar:', error));
+         }
+        
+        window.onload = function() {
+            carregarProdutos();
+        }
+        
+
+</script>
+</body>
+
+</html>
